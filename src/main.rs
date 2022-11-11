67810d90a43ce6f5a894c2ef0c1356d5c297483b
314ShadePi <erikkirkegaardstage@gmail.com>
2022-11-11 13:24:20 +0100

    It might work

--- src/main.rs
+++ src/main.rs
@@ -3,10 +3,19 @@ use actix_web::{get, web, App, HttpResponse, HttpServer, Responder};
 use std::sync::Arc;
 use tokio::sync::Mutex;
 
-#[derive(PartialEq, Eq)]
+#[derive(PartialEq, Eq, Copy, Clone)]
 enum CStatus {
     Locked,
     UnLocked,
+}
+
+impl std::fmt::Display for CStatus {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        match self {
+            CStatus::Locked => write!(f, "LOCKED"),
+            CStatus::UnLocked => write!(f, "UNLOCKED"),
+        }
+    }
 }
 
 #[actix_web::main]
@@ -30,6 +39,10 @@ async fn main() -> std::io::Result<()> {
     .await
 }
 
+async fn get_status(config: web::Data<Arc<Mutex<CStatus>>>) -> CStatus {
+    *config.lock().await
+}
+
 #[get("/unlock")]
 async fn unlock(config: web::Data<Arc<Mutex<CStatus>>>) -> impl Responder {
     let mut c_lock = config.lock().await;
@@ -48,26 +61,14 @@ async fn lock(config: web::Data<Arc<Mutex<CStatus>>>) -> impl Responder {
 
 #[get("/ring")]
 async fn ring(config: web::Data<Arc<Mutex<CStatus>>>) -> impl Responder {
-    let c_lock = config.lock().await;
-
-    if *c_lock == CStatus::Locked {
-        println!("RING - LOCKED");
-        HttpResponse::Ok().body("LOCKED")
-    } else {
-        println!("RING - UNLOCKED");
-        HttpResponse::Ok().body("UNLOCKED")
-    }
+    let c_status = get_status(config).await.to_string();
+    println!("RING - {}", c_status.clone());
+    HttpResponse::Ok().body(c_status)
 }
 
 #[get("/status")]
 async fn status(config: web::Data<Arc<Mutex<CStatus>>>) -> impl Responder {
-    let c_lock = config.lock().await;
-
-    if *c_lock == CStatus::Locked {
-        println!("STATUS - LOCKED");
-        HttpResponse::Ok().body("LOCKED")
-    } else {
-        println!("STATUS - UNLOCKED");
-        HttpResponse::Ok().body("UNLOCKED")
-    }
+    let c_status = get_status(config).await.to_string();
+    println!("STATUS - {}", c_status.clone());
+    HttpResponse::Ok().body(c_status)
 }
